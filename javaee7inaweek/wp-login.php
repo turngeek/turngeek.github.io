1bc582ec5ea01fb81965639f20cfea374105d8da
Marcus Schiesser <mail@marcusschiesser.de>
2018-10-10 18:26:26 +0800

    changed root to / and removed CNAME

--- javaee7inaweek/wp-login.php
+++ javaee7inaweek/wp-login.php
@@ -8,13 +8,13 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Cloud Tutorial Series &#8211; Java EE 7 &rsaquo; Log In</title>
-	<link rel='stylesheet' id='buttons-css'  href='http://www.turngeek.press/javaee7inaweek/wp-includes/css/buttons.min.css?ver=4.4' type='text/css' media='all' />
+	<link rel='stylesheet' id='buttons-css'  href='/javaee7inaweek/wp-includes/css/buttons.min.css?ver=4.4' type='text/css' media='all' />
 <link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.4' type='text/css' media='all' />
-<link rel='stylesheet' id='dashicons-css'  href='http://www.turngeek.press/javaee7inaweek/wp-includes/css/dashicons.min.css?ver=4.4' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='http://www.turngeek.press/javaee7inaweek/wp-admin/css/login.min.css?ver=4.4' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='/javaee7inaweek/wp-includes/css/dashicons.min.css?ver=4.4' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='/javaee7inaweek/wp-admin/css/login.min.css?ver=4.4' type='text/css' media='all' />
 <style type="text/css">
 .login h1 a {
-  background-image: url(http://www.turngeek.press/javaee7inaweek/wp-content/plugins/pressbooks/assets/images/PB-logo.svg);
+  background-image: url(/javaee7inaweek/wp-content/plugins/pressbooks/assets/images/PB-logo.svg);
   background-size: 276px 40px;
   width: 276px;
   height: 40px; }
@@ -24,15 +24,15 @@
   color: #d4002d; }
 
 .no-svg .login h1 a {
-  background-image: url(http://www.turngeek.press/javaee7inaweek/wp-content/plugins/pressbooks/assets/images/PB-logo.png); }
+  background-image: url(/javaee7inaweek/wp-content/plugins/pressbooks/assets/images/PB-logo.png); }
 </style>
-<link rel="stylesheet" type="text/css" href="http://www.turngeek.press/javaee7inaweek/wp-content/plugins/pressbooks/assets/css/colors-pb.css" media="screen" /><meta name='robots' content='noindex,follow' />
+<link rel="stylesheet" type="text/css" href="/javaee7inaweek/wp-content/plugins/pressbooks/assets/css/colors-pb.css" media="screen" /><meta name='robots' content='noindex,follow' />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 	<div id="login">
-		<h1><a href="http://www.turngeek.press/javaee7inaweek" title="Cloud Tutorial Series - Java EE 7" tabindex="-1">Cloud Tutorial Series &#8211; Java EE 7</a></h1>
+		<h1><a href="/javaee7inaweek" title="Cloud Tutorial Series - Java EE 7" tabindex="-1">Cloud Tutorial Series &#8211; Java EE 7</a></h1>
 	
-<form name="loginform" id="loginform" action="http://www.turngeek.press/javaee7inaweek/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="/javaee7inaweek/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -44,13 +44,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="http://www.turngeek.press/javaee7inaweek/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="/javaee7inaweek/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="http://www.turngeek.press/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+	<a href="/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -67,7 +67,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://www.turngeek.press/javaee7inaweek/" title="Are you lost?">&larr; Back to Cloud Tutorial Series &#8211; Java EE 7</a></p>
+	<p id="backtoblog"><a href="/javaee7inaweek/" title="Are you lost?">&larr; Back to Cloud Tutorial Series &#8211; Java EE 7</a></p>
 	
 	</div>
 
